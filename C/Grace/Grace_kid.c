2d2917aacdc8107f4884f506dcc3c559f63a3e65
Youness Benbraitit <benbraitit@gmail.com>
2022-11-20 13:11:18 +0100

    done

--- C/Grace/Grace_kid.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#define FILE "Grace_kid.c"
-#define CODE "#include <stdio.h>%c#define FILE %c%s%c%c#define CODE %c%s%c%c#define FT(x)int main(){fprintf(fopen(FILE, x), CODE, 10, 34, FILE, 34, 10, 34, CODE, 34, 10, 10, 10, 34, 119, 34, 10);}%c// Comment%cFT(%c%c%c)%c"
-#define FT(x)int main(){fprintf(fopen(FILE, x), CODE, 10, 34, FILE, 34, 10, 34, CODE, 34, 10, 10, 10, 34, 119, 34, 10);}
-// Comment
-FT("w")
