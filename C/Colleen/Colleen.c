aa35a46b5c53bed2d52aa420eb1f4efc59751536
Youness Benbraitit <benbraitit@gmail.com>
2022-11-20 20:36:14 +0100

    changed comments type

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-// print function
+/*
+* outside comment
+*/
 void print(char *s){printf(s, 10, 34, s);}
 int main(){
-// the String to print
-char *s = "#include <stdio.h>%1$c// print function%1$cvoid print(char *s){printf(s, 10, 34, s);}%1$cint main(){%1$c// the String to print%1$cchar *s = %2$c%s%2$c;%1$cprint(s);%1$creturn (0);}";
+/*
+* inside comment
+*/
+char *s = "#include <stdio.h>%1$c/*%1$c* outside comment%1$c*/%1$cvoid print(char *s){printf(s, 10, 34, s);}%1$cint main(){%1$c/*%1$c* inside comment%1$c*/%1$cchar *s = %2$c%s%2$c;%1$cprint(s);%1$creturn (0);}";
 print(s);
 return (0);}
